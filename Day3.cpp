903210e16e895484431774a11b8a3bef98918149
Lorraine <32841084+lorraineRu@users.noreply.github.com>
2018-11-23 14:06:53 +0000

    Day3.cpp

---
+++ Day3.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int N;
+    cin >> N;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    if (N % 2 == 0) 
+    { 
+        if (2 < N < 5) 
+        {
+            cout << "Not Weird" << endl;
+        }
+        else if (6 < N < 20)
+        {
+            cout << "Weird" << endl;
+        }
+        else 
+        {
+            cout << "Not Weird" << endl;
+        }
+    }
+    else {cout << "Weird" << endl;}
+
+    return 0;
+}
